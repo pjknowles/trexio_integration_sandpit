878a1d481573908f1138c2b07b1b6d611b0a6a08
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2025-07-10 17:17:51 +0100

    test

--- main.F90
+++ main.F90
@@ -1,5 +1,8 @@
-#include <trexio_f.f90>
-!#include "trexio_f.f90"
 program trexio_integration_sandpit
-    write(*,*) "Hello, World!"
+#include <my_header.f90>
+!    use my_module, only : my_module_message => message
+    use trexio
+    call trexio_assert(1, 1, "success")
+!    write(*, *) my_module_message
+    write(*, *) message
 end program
---
+++ my_module/my_module.f90
@@ -0,0 +1,3 @@
+module my_module
+  character(*), parameter :: message="Hi!"
+end module my_module
